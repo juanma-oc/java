57aacfc8bd7ce1e99fd6d9c47512be2355f3ee34
juanma-oc <juanorellanacalvillo@gmail.com>
2025-10-20 18:44:22 +0200

    primer

---
+++ prueba2.java
@@ -0,0 +1,7 @@
+import ES.ES;
+public class prueba1 {
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+        int a = ES.leeN("Introduce el numero");
+    }
+}
